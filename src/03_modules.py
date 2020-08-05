e3989af648dce520349e248d51cbb1b6ae808858
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-08-05 19:42:24 -0400

    files 00-09 done

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,10 +10,8 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# import fileinput
 
-# for line in fileinput.input():
-#     print(line)
+print(sys.argv)  # get clarification on this one?
 # Print out the OS platform you're using:
 # YOUR CODE HERE
 x = sys.platform
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -16,25 +16,14 @@ has the following keys:
 """
 
 waypoints = [
-    {
-        "lat": 43,
-        "lon": -121,
-        "name": "a place"
-    },
-    {
-        "lat": 41,
-        "lon": -123,
-        "name": "another place"
-    },
-    {
-        "lat": 43,
-        "lon": -122,
-        "name": "a third place"
-    }
+    {"lat": 43, "lon": -121, "name": "a place"},
+    {"lat": 41, "lon": -123, "name": "another place"},
+    {"lat": 43, "lon": -122, "name": "a third place"},
 ]
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({"lat": 44, "lon": 143, "name": "a newplace"})
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +31,8 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0].update({"lon": -130, "name": "not a real place"})
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+print(waypoints)
